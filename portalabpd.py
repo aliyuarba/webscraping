d59094afd07a4af06545225f5de724116fa3c078
aliyuarba <arba313@gmail.com>
2023-02-25 08:37:36 +0700

    add code

--- portalabpd.py
+++ portalabpd.py
@@ -1,15 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+arr = []
 
-# Mengirimkan permintaan GET ke URL yang ditentukan
-url = "https://djpk.kemenkeu.go.id/portal/data/apbd?tahun=2022&provinsi=11&pemda=21"
-response = requests.get(url)
+tahun = np.arange(2011, 2013)
+for th in tahun: 
 
-# Menggunakan BeautifulSoup untuk memproses HTML dan menyimpannya dalam variabel soup
-soup = BeautifulSoup(response.content, 'lxml')
+  # Mengirimkan permintaan GET ke URL yang ditentukan
+  url = "https://djpk.kemenkeu.go.id/portal/data/apbd?tahun=2021&provinsi=11&pemda=21".format(th)
+  response = requests.get(url)
 
-# Mencari elemen yang ingin diambil dari halaman web
-tabel = soup.find('tbody')
+  # Menggunakan BeautifulSoup untuk memproses HTML dan menyimpannya dalam variabel soup
+  soup = BeautifulSoup(response.content, 'lxml')
 
-# Menampilkan hasil
-print(tabel.prettify())
+
+  # Mencari elemen yang ingin diambil dari halaman web
+  tabel = soup.find('tbody')
+  for tr in tabel.find_all('tr'):
+    rows = [th]
+    if tr.text.strip() != "":
+      for td in tr.find_all('td'):
+        if td.text.strip() != "":
+          rows.append(td.text)
+      arr.append(rows)
+
+# Membuat DataFrame dari array
+df = pd.DataFrame(arr)
+
+# Menyimpan DataFrame ke dalam file CSV
+df.to_csv('pwr2022.csv', index=False)
+
+# Menampilkan DataFrame
+print(df)
